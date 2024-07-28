5c70d34f275449b895bdd029fafcba06264abcbc
al-kuresh <kureshreush@gmail.com>
2024-07-28 18:46:09 +0600

    Nested Dictionary

--- fOURTH_DICTIONARY_SET.py
+++ fOURTH_DICTIONARY_SET.py
@@ -15,6 +15,27 @@ print(Info["Name"])     #we can print dictionary values separately by Info["Key"
 print(Info["Subjects"])
 
 Info["Name"] = "Faisal"
-Info["Address"] = "Amlapara,Narayangonj"
+Info["Address"] = "Amlapara,Narayangonj"  # We can add a new Key in the Dictionary by assigning this way
 print("   ")
 print(Info)
+
+# Nested Dictionary
+
+NSU = {
+    "Student1" : "Saif",
+    "Marks1" : {
+        "phy"   : 98,
+        "che"   : 95,
+        "Math"  : 90
+    },
+    "Student2" : "Ratan",
+    "Marks2" : {
+        "Phy" : 84,
+        "Che" : 76,
+        "Math": 100
+    },
+}
+
+print(NSU)
+print("  ")
+print(NSU["Marks1"]["Math"])
