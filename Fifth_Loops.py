be3c148cad901f791cd1345f56549c31cdc031bd
al-kuresh <kureshreush@gmail.com>
2024-07-30 21:50:03 +0600

    range is learnt

--- Fifth_Loops.py
+++ Fifth_Loops.py
@@ -87,25 +87,80 @@ str = "North South Univarsity"
 for char in str:
     print(char)
 
-# Searching a number in tupple
-various = []
-size = int (input("Enter the size of the List: "))
+# # Searching a number in tupple
+# various = []
+# size = int (input("Enter the size of the List: "))
+# i=1
+# while i<=size:
+#     x = int (input("enter number: "))
+#     various.append(x)
+#     i+=1
+
+# tp = tuple(various)
+# print("Printing the tuppple without for loop:-",tp)
+# x= int (input("Searching number: "))
+# count=0
+
+# for val in tp:
+#     if(val == x):
+#         count=1
+
+# if(count==1):
+#     print(x,"number is found")
+# else:
+#     print(x,"number is not found")    
+
+# Rangre(start,stop,step) , it starts from 0, returns a sequence of nums 
+print(range(10)) # returns (0,10)
+serial = range(4)   # this serial only carries value from 0 to 4. nothing else
+print(serial[2])
+print(serial[3])
+
+serial = range(5)
+for i in serial:
+    print(i)
+
+# we can directly use the range 
+for i in range(100):
+    print("Numbers: ",i+1)
+
+# (start,stop, step)
+for i in range(1,6): # prints from start: 1 to Ends:  5
+    print(i)
+print("     ")
+
+for i in range(1,10,2): # prints from start: 1 ,Ends:  9 , steps : 2
+    print(i)
+print("     ")
+
+#  prints from start: 100 ,Ends:  1 , steps : -1
+for i in range(100,0,-1):
+    print(i)
+print("     ")
+
+# Multiplication of Table number n
+num  = int (input("Enter to multiply: "))
+for i in range(1,11):
+    print(num*i)
+
+# Pass Statement it does nothing , just passes 
+for el in range(1,10):
+    pass
+
+print("Did nothing")
+
+# the sum of first n numbers
+n = int (input("Enter n(Sum): "))
 i=1
-while i<=size:
-    x = int (input("enter number: "))
-    various.append(x)
+sum=0
+while i<=n:
+    sum+=i
     i+=1
+print("Sum : ",sum)
 
-tp = tuple(various)
-print("Printing the tuppple without for loop:-",tp)
-x= int (input("Searching number: "))
-count=0
-
-for val in tp:
-    if(val == x):
-        count=1
-
-if(count==1):
-    print(x,"number is found")
-else:
-    print(x,"number is not found")    
+# Factorial
+n = int (input("Enter n(Fact): "))
+fact=1
+for i in range(1,n+1):
+    fact*=i
+print(fact)
