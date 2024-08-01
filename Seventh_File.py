9b801f87d89e7c38eaf55b0541e247f8349f0527
al-kuresh <kureshreush@gmail.com>
2024-08-01 15:27:59 +0600

    with

--- Seventh_File.py
+++ Seventh_File.py
@@ -21,12 +21,32 @@ print("\n")
 
 f =open("D:\Python Folder\prototype.txt","r")
 text1  = f.read(10) # read the first 10 characters of the file
-print(text1,"\n") 
+print(text1,"\n")
 f.close()
 
 f =open("D:\Python Folder\prototype.txt","r")
 l1 = f.readline() # reads the first whole line of the file
-l2 = f.readline()
+l2 = f.readline() # reads the second whole line of the file
 print(l1)
 print(l2)
 f.close()
+
+# Writing something in a file
+f = open("D:\Python Folder\prototype.txt","w")
+f.write("The next thing I am going to learn is Html and Css")
+f.write("\n123")
+f.write("\nDjango as well")
+f.close()
+
+# Appending in a file
+f = open("D:\Python Folder\prototype.txt","a")
+f.write("\nThen I will proceed to a project")
+f.close()
+
+#Reading the entire file again
+with open("D:\Python Folder\prototype.txt","r") as f:
+    doc = f.read()
+    print(doc)
+
+with open("D:\Python Folder\prototype.txt","w") as f:
+    f.write("we use the with function")
