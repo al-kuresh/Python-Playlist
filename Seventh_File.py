b6071c6d7f82fa0df78d443000c446881525247c
al-kuresh <kureshreush@gmail.com>
2024-08-01 11:50:31 +0600

    newline

--- Seventh_File.py
+++ Seventh_File.py
@@ -1,6 +1,32 @@
 # f = open("File_name","mode")
+# Modes 
+"""
+"r" for reading a file
+"w" for overwrite the file and write new things
+"a" for append ( writing in a existing file from the last input)
+"x" for creating a new file and write it
+"b' for binary mode (rb,wb,ab)
+"+" for reading and writing both (r+,w+,a+)
+"""
+
+# f.read() reads the entire file
+# f.readline() reads the one line at  a time
 
 f  = open("D:\Python Folder\prototype.txt","r")
 text = f.read()
 print(text)
+f.close()
+
+print("\n")
+
+f =open("D:\Python Folder\prototype.txt","r")
+text1  = f.read(10) # read the first 10 characters of the file
+print(text1,"\n") 
+f.close()
+
+f =open("D:\Python Folder\prototype.txt","r")
+l1 = f.readline() # reads the first whole line of the file
+l2 = f.readline()
+print(l1)
+print(l2)
 f.close()
